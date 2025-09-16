846c7b1c05b9836261e8b6c7a48ab732e1d48493
abdelbaset404 <abdelbasite2301.sayed@gmail.com>
2025-09-16 22:08:45 +0300

    Update settings.py

--- hr_system/settings.py
+++ hr_system/settings.py
@@ -83,12 +83,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'hr_system.wsgi.application'
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+ DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+     }
+ }
 
 
 # Password validation
